574ef685b089ac912dd35c8f769a8965cdb651e5
SYSolakov20 <SYSolakov20@codingburgas.bg>
2023-07-02 22:05:42 +0300

    Add background image

--- sunmo/lib/currentWeather.dart
+++ sunmo/lib/currentWeather.dart
@@ -15,7 +15,13 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background-worm.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: FutureBuilder<Weather>(
           builder: (context, snapshot) {
             if (snapshot.hasData) {
@@ -24,7 +30,7 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
             } else if (snapshot.hasError) {
               return Text("Error fetching weather");
             } else {
-              return CircularProgressIndicator(); // Show a loading indicator while fetching data
+              return Center(child: CircularProgressIndicator());
             }
           },
           future: getCurrentWeather(),
@@ -34,39 +40,37 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   }
 
   Widget weatherBox(Weather _weather) {
-    return Column(
-      children: <Widget>[
-        Text("${_weather.temp}"),
-        Text("${_weather.description}"),
-        Text("${_weather.feelsLike}"),
-        Text("${_weather.low}"),
-        Text("${_weather.high}"),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("${_weather.temp}"),
+          Text("${_weather.description}"),
+          Text("${_weather.feelsLike}"),
+          Text("${_weather.low}"),
+          Text("${_weather.high}"),
+        ],
+      ),
     );
   }
 
   Future<Weather> getCurrentWeather() async {
     String apiKey = "230a8144b2272a1e71dc0aaa1cea83e7";
-    double latitude = 33.44;
-    double longitude = -94.04;
-    String exclude = "hourly,daily";
+    double latitude = 42.510578;
+    double longitude = 27.461014;
+    String exclude = "hourly";
 
     var url = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=42.510578&lon=-27.461014&exclude=hourly&appid=230a8144b2272a1e71dc0aaa1cea83e7");
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&exclude=$exclude&appid=$apiKey");
 
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-  Map<String, dynamic> json = jsonDecode(response.body);
-  print(json); // Log the JSON response
-  Weather weather = Weather.fromJson(json);
-  print(weather); // Log the Weather object
-  return weather;
-} else {
-  print('Failed to load weather. Status code: ${response.statusCode}');
-  throw Exception('Failed to load weather');
-}
+      Map<String, dynamic> json = jsonDecode(response.body);
+      Weather weather = Weather.fromJson(json);
+      return weather;
+    } else {
+      throw Exception('Failed to load weather');
+    }
   }
-  
 }
-
